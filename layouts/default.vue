02520b1f3c9591721e1332c04ec98f66c3fbf71d
Francisco <JoseFranciscoPaez1999@gmail.com>
2024-08-08 17:41:13 -0300

    un poco de incorporacion de SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,10 @@
 <template>
+    <head>
+        <title>Mi Portafolio - Diseñador y Desarrollador Web</title>
+        <meta name="description"
+            content="Descubre mi portafolio de proyectos web, diseño y desarrollo. Conoce mis habilidades y servicios como diseñador y desarrollador web.">
+        <meta name="keywords" content="portafolio, diseño web, desarrollo web, proyectos, diseñador, desarrollador">
+    </head>
     <div>
         <Navbar />
         <HeroSection />
@@ -10,13 +16,11 @@
 </template>
 
 <script setup>
-    import Navbar from '~/components/Navbar.vue'
-    import HeroSection from '~/components/HeroSection.vue';
-    import AboutMe from '~/components/AboutMe.vue';
-    import Projects from '../components/Projects.vue';
-    import Contact from '~/components/Contact.vue';
+import Navbar from '~/components/Navbar.vue'
+import HeroSection from '~/components/HeroSection.vue';
+import AboutMe from '~/components/AboutMe.vue';
+import Projects from '../components/Projects.vue';
+import Contact from '~/components/Contact.vue';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
